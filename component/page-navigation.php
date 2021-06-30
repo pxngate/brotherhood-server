0076bcc8d499a69f3f8e6cfe1245e96ef0dde68b
pxngate <pxngate@email.cz>
2021-06-30 21:51:48 +0200

    Converting to PHP

--- component/page-navigation.php
+++ component/page-navigation.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brootherhood | Domů</title>
-    <link rel="stylesheet" href="style/default/reset.css">
-    <link rel="stylesheet" href="style/default/font.css">
-    <link rel="stylesheet" href="style/default/classes.css">
-    <link rel="stylesheet" href="style/main.css">
-    <link rel="stylesheet" href="style/component/nav.css">
-    <script defer src="node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
-    <script defer src="node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
-    <script defer src="node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
-</head>
-<body>
-    <nav class="page-navigation">
+<?php
+    echo '<nav class="page-navigation">
         <div class="nav-content">
             <div class="nav-logo bg-primary"></div>
             <div class="nav-hider">
@@ -44,13 +28,6 @@
                                 <div class="image-overlay"></div>
                             </div>
                             <p class="box-label">Box 2</p>
-                        </div>
-                        <div class="item-box">
-                            <div class="box-image">
-                                <div class="image-content" style=""></div>
-                                <div class="image-overlay"></div>
-                            </div>
-                            <p class="box-label">Box 3</p>
                         </div>
                     </div>
                 </li>
@@ -139,18 +116,5 @@
 
         </div>
     </nav>
-    <header class="page-header centered">
-        <div class="header-content">
-            <div class="header-title">
-                <h1 class="title-line">brotherhood</h1>
-                <h1 class="title-line stroke">server.</h1>
-            </div>
-            <div class="header-ctas">
-                <a href="#" class="header-cta bd-white">O serveru</a>
-                <a href="#" class="header-cta bg-primary">Začít hrát</a>
-            </div>
-        </div>
-    </header>
-    <script src="script/nav.js"></script>
-</body>
-</html>
+    ';
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brootherhood | Domů</title>
+    <link rel="stylesheet" href="style/default/reset.css">
+    <link rel="stylesheet" href="style/default/font.css">
+    <link rel="stylesheet" href="style/default/classes.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/component/nav.css">
+    <script defer src="node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
+    <script defer src="node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
+    <script defer src="node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
+</head>
+<body>
+    <?php
+        include 'component/page-navigation.php';
+    ?>
+    </nav>
+    <header class="page-header centered">
+        <div class="header-content">
+            <div class="header-title">
+                <h1 class="title-line">brotherhood</h1>
+                <h1 class="title-line stroke">server.</h1>
+            </div>
+            <div class="header-ctas">
+                <a href="#" class="header-cta bd-white">O serveru</a>
+                <a href="#" class="header-cta bg-primary">Začít hrát</a>
+            </div>
+        </div>
+    </header>
+    <script src="script/nav.js"></script>
+</body>
+</html>
